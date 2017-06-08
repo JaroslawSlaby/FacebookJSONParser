3fa3f230803f16df50a7aa70aa333edba4aed85b
Jaroslaw Slaby <jaroslaw.slaby@gmail.com>
2017-06-08 11:35:48 +0200

    Refactor in testing class

--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -1,29 +1,20 @@
 import classes.JSONParser;
-import classes.structure.Profile;
-import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by jaroslaw on 08.06.17.
  */
 public class JSONParserTest {
 
-    private List<Profile> profiles;
-    private Profile profile;
-    private Gson gson;
     private JSONParser jsonParser;
 
     @Before
     public void initObjects() {
         jsonParser = new JSONParser(".");
-        profiles = new ArrayList<Profile>();
 
     }
 
